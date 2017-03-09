5d38d235e1245ef682d28d7ad9b830cb0726e4e4
Daniel <mrdanielkang@gmail.com>
2017-03-09 15:27:51 -0800

    Added play/pause for all and load more

--- assets/js/something.js
+++ assets/js/something.js
@@ -1,49 +1,69 @@
 window.onload=start;
+
 var topics = ["Tyrion Lannister", "Daenerys Targaryen", "Rick and Morty", "Doctor Who"];
+var picNum = 0;
+var results;
 
 function start() {
 	for (var i = 0; i < topics.length; i++) {
-		var btn = $("<button>");
-		btn.attr("data-name", topics[i]);
-		btn.text(topics[i]);
-		$("#top").append(btn);
+		addButton(topics[i]);
 	}
+	$("#playPause").hide();
+}
+
+function addButton(i) {
+	var btn = $("<button>");
+	btn.attr("data-name", i);
+	btn.text(i);
+	$("#top").append(btn);
 }
 
 $("#top").on("click", "button", function() {
+	picNum = 0;
 	$("#images").empty();
 	imageLoad($(this).attr("data-name"));
 });
 
 $("#search-submit").on("click", function(event) {
 	event.preventDefault();
+	picNum = 0;
 	$("#images").empty();
-	imageLoad($("#search-input").val().trim());
+	var search = $("#search-input").val().trim();
+	imageLoad(search);
+	addButton(search);
+	$("#search-input").val("");
 });
 
 function imageLoad(look) {
-	publicKey = "&api_key=dc6zaTOxFJmzC&limit=10";
+	publicKey = "&api_key=dc6zaTOxFJmzC";
 	queryURL = "http://api.giphy.com/v1/gifs/search?q=" + look + publicKey;
 
 	$.ajax({
 		url:queryURL,
 		method: "GET"
 	}).done(function(response) {
-		var results = response.data;
-		for (var i = 0; i < 10; i++) {
-			var gifDiv = $("<div class='xD'>");
-			var p = $("<p>").text("Rating: " + results[i].rating);
-			var image = $("<img>");
-			image.attr("src", results[i].images.fixed_height_still.url);
-			image.attr("data-state", "still");
-			image.attr("data-still", results[i].images.fixed_height_still.url);
-			image.attr("data-animate", results[i].images.fixed_height.url);
-			$(image).addClass("gif");
-			gifDiv.prepend(p);
-			gifDiv.prepend(image);
-			$("#images").prepend(gifDiv);
-		}
+		results = response.data;
+		
+		load();
 	});
+}
+
+function load() {
+	$("#playPause").show();
+	for (var i = 0; i < 10; i++) {
+		var gifDiv = $("<div class='xD'>");
+		var p = $("<p>").text("Rating: " + results[picNum].rating);
+		var image = $("<img>");
+		image.attr("src", results[picNum].images.fixed_height_still.url);
+		image.attr("data-state", "still");
+		image.attr("data-still", results[picNum].images.fixed_height_still.url);
+		image.attr("data-animate", results[picNum].images.fixed_height.url);
+		$(image).addClass("gif");
+		gifDiv.prepend(p);
+		gifDiv.prepend(image);
+		$("#images").prepend(gifDiv);
+		picNum++;
+	}
 }
 
 $("#images").on("click", "img", function() {
@@ -59,4 +79,18 @@ function imageClick(x) {
 		$(x).attr("src", $(x).attr("data-still"));
 		$(x).attr("data-state", "still");
 	}
+}
+
+function playAll() {
+	$(".gif").each(function() {
+		$(this).attr("src", $(this).attr("data-animate"));
+		$(this).attr("data-state", "animate");
+	});
+}
+
+function stopAll() {
+	$(".gif").each(function() {
+		$(this).attr("src", $(this).attr("data-still"));
+		$(this).attr("data-state", "still");
+	});
 }
